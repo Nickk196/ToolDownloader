3bc57dd7975e980e1860de3b7abd3fb526ae9d28
Nicccc <megeleanicolas@gmail.com>
2026-05-28 00:55:48 +0300

    Create ToolDownloader.ps1

---
+++ ToolDownloader.ps1
@@ -0,0 +1,31 @@
+# List of URLs to download
+ $urls = @(
+    "https://github.com/Orbdiff/SSTool/releases/download/yay/SSTool.exe",
+    "https://github.com/MeowTonynoh/MeowDoomsdayFucker/releases/download/V.1.2/MeowDoomsdayFucker.exe",
+    "https://github.com/MeowTonynoh/MeowImportsChecker/releases/download/MeowImportsChecker/MeowImportsChecker.exe",
+    "https://github.com/MeowTonynoh/MeowResolver/releases/download/MeowResolver/MeowResolver.exe",
+    "https://github.com/horsicq/DIE-engine/releases/download/3.09/die_win64_portable_3.09_x64.zip",
+    "https://www.nirsoft.net/utils/winprefetchview.zip",
+    "https://www.nirsoft.net/utils/userassistview.zip",
+    "https://www.nirsoft.net/utils/taskschedulerview.zip",
+    "https://www.nirsoft.net/utils/computeractivityview.zip"
+)
+
+# Loop through each URL and download
+foreach ($url in $urls) {
+    # Extract the file name from the URL
+    $fileName = Split-Path $url -Leaf
+    
+    Write-Host "Downloading $fileName..." -ForegroundColor Cyan
+    
+    try {
+        # Download the file to the current directory
+        Invoke-WebRequest -Uri $url -OutFile $fileName
+        Write-Host "Success: $fileName" -ForegroundColor Green
+    }
+    catch {
+        Write-Host "Failed to download $fileName" -ForegroundColor Red
+    }
+}
+
+Write-Host "All downloads finished." -ForegroundColor Yellow
